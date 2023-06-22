ce5107c5c5b6471dce3d9a1f3d26a41bd4c903e2
Guilherme Martins <academicgmag@gmail.com>
2023-06-21 21:08:26 -0300

    add link wrapper component

---
+++ src/components/LinkWrapper/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+import Link from 'next/link'
+
+type LinkWrapperProps = {
+  href: string
+  children: React.ReactNode
+}
+
+export const LinkWrapper = ({ href, children }: LinkWrapperProps) => (
+  <S.Wrapper>
+    <Link href={href}>{children}</Link>
+  </S.Wrapper>
+)
